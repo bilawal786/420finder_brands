a5201c8cff398fb4bd04ad0a2d6d003e5d340e75
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-09-20 11:40:33 +0500

    enw

--- resources/views/business/index.blade.php
+++ resources/views/business/index.blade.php
@@ -47,7 +47,7 @@
                           </div>
                         </div>
                       </div>
-                      <div class="col-sm-6 col-md4 mb-3">
+                      <div class="col-sm-6 col-md-4 mb-3">
                         <div class="card business-card">
                           <h5>Deliveries</h5>
                           <div class="card-content">
