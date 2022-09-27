286f97f4d6aaec4efd76db358723e739fb56d1b9
mohamedbilawal <bilawal.jabbar.786@gmail.com>
2022-09-27 18:56:52 +0500

    enw

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -5,24 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Business;
 use Illuminate\Http\Request;
 
-class StoreController extends Controller {
-
-    public function index() {
-
+class StoreController extends Controller
+{
+    public function index()
+    {
         $brands = Brand::where('status', 1)->select('id', 'name')->get();
-
         return view('requestproducts.index')
             ->with('brands', $brands);
-
     }
 
-    public function stores() {
-
-        $stores = Business::where('email', session('business_email'))->where('business_type', '!=', 'Brand')->get();
-
+    public function stores()
+    {
+        $stores = Business::where('email', session('business_email'))->where('email', '!=', null)->where('business_type', '!=', 'Brand')->get();
         return view('business.stores.index')
-          ->with('stores', $stores);
-
+            ->with('stores', $stores);
     }
-
 }
